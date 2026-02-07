7138ef4f492e72f778a8d3ebde140c3999542cda
pranav <pranavmarke66@gmail.com>
2026-02-07 15:19:51 +0530

    hhhh

---
+++ script2.js
@@ -0,0 +1,133 @@
+let ques = document.querySelector(".ques")
+let next = document.querySelector(".next")
+let option1 = document.querySelector("#option1")
+let option2 = document.querySelector("#option2")
+let option3 = document.querySelector("#option3")
+let option4 = document.querySelector("#option4")
+let options = document.querySelectorAll(".options")
+let scoree = document.querySelector(".score")
+let box = document.querySelector(".box")
+let submit = document.querySelector(".submit")
+let start = document.querySelector(".start")
+let index = 0;
+
+let arr = ["1. Which is the largest planet in our Solar System?", "2. Who is known as the “Father of the Indian Constitution”?", "3. Which gas is most abundant in the Earth's atmosphere", "4. What is the national animal of India?"]
+
+let q = [
+    {
+        option: ["Earth", "mars", "pluto", "jupiter"],
+        correct: 4
+    },
+    {
+        option: ["mahatma gandhi", " narendrea modi ", "br ambedkar", "pranav marke"],
+        correct: 3
+    },
+    {
+        option: ["iodine", "co2", "nitrogen", "oxygen"],
+        correct: 3
+    },
+    {
+        option: ["lion", "tiger", "dog", "cat"],
+        correct: 2
+    }
+]
+
+
+//taake answer from the user 
+let answerindex = []
+function takeanswer() {
+    options.forEach((opt, i) => {
+        opt.addEventListener("click", () => {
+            answerindex[index - 1] = i + 1;
+
+        })
+    })
+}
+
+function putqandans() {
+    ques.innerHTML = arr[index];
+    option1.innerHTML = q[index].option[0]
+    option2.innerHTML = q[index].option[1]
+    option3.innerHTML = q[index].option[2]
+    option4.innerHTML = q[index].option[3]
+}
+
+
+start.addEventListener("click", () => {
+    options.forEach(element => {
+        element.style.display = "flex"
+    });
+    putqandans()
+    next.style.display = "flex"
+    start.style.display = "none"
+    takeanswer()
+    index++
+
+})
+
+
+next.addEventListener("click", () => {
+
+    if (index < (arr.length - 1)) {
+        if (answerindex[index - 1]) {
+            options.forEach(element => {
+                element.style.display = "flex"
+
+
+            });
+
+            next.innerHTML = "next"
+            putqandans()
+            takeanswer()
+            index++;
+        }
+        else {
+            alert("u havent select")
+        }
+
+    }
+    else if (index == (arr.length - 1)) {
+        if (answerindex[index - 1]) {
+
+
+            submit.style.display = "flex"
+            options.forEach(element => {
+                element.style.display = "flex"
+            });
+
+            next.innerHTML = "next"
+            putqandans()
+            takeanswer()
+            index++;
+            next.style.display = "none"
+            next.disabled = true
+        }
+        else{
+            alert("u havent select")
+        }
+    }
+    else {
+        box.innerHTML = `your score is ${score} `
+    }
+})
+
+submit.addEventListener("click", () => {
+    console.log(answerindex)
+    checkanswer()
+    console.log(score)
+})
+
+let score = 0;
+function checkanswer() {
+    for (let i = 0; i < answerindex.length; i++) {
+        if (answerindex[i] == q[i].correct) {
+            score++;
+
+        }
+
+
+    }
+    return score
+}
+
+
